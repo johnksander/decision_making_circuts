b56684d7255210c0f966568df913ee14409c377c
johnksander <john.ksander@gmail.com>
2019-08-26 12:22:59 -0400

    20190826

--- Simulation/nets_D2t_pref_driver.m
+++ Simulation/nets_D2t_pref_driver.m
@@ -10,12 +10,12 @@ format compact
 jID = str2num([getenv('SLURM_JOBID'), getenv('SLURM_ARRAY_TASK_ID')]);
 t = 350; %trial simulation time (s) 
 options = set_options('modeltype','NETS','comp_location','hpc',...
-    'sim_name','nets_D2t_pref','jobID',jID,'tmax',t,...
-    'netpair_file','D2t','record_spiking','off');
+    'sim_name','nets_D2t_20s_pref','jobID',jID,'tmax',t,...
+    'netpair_file','D2t_20s','record_spiking','off');
 
 
 %adjust stimulus B strength
-stim_mod = 0:.25:.25; % 0:.25:2; %just randomly sample mod weight, do enough it'll even out 
+stim_mod = 0:.25:2; % 0:.25:2; %just randomly sample mod weight, do enough it'll even out 
 stim_mod = randsample(stim_mod,1);
 options.trial_stimuli(2) = options.trial_stimuli(2) * stim_mod; %adjust stim B
 %---run-----------------------
