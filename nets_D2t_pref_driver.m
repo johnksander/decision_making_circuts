9fff3c75699859c3626f6d1f89b9b6373c2a694f
johnksander <john.ksander@gmail.com>
2019-09-30 14:52:10 -0400

    20190930

--- nets_D2t_pref_driver.m
+++ nets_D2t_pref_driver.m
@@ -10,8 +10,8 @@ format compact
 jID = str2num([getenv('SLURM_JOBID'), getenv('SLURM_ARRAY_TASK_ID')]);
 t = 350; %trial simulation time (s) 
 options = set_options('modeltype','NETS','comp_location','hpc',...
-    'sim_name','nets_D2t_20s_pref','jobID',jID,'tmax',t,...
-    'netpair_file','D2t_20s','record_spiking','off');
+    'sim_name','nets_D2t_pref','jobID',jID,'tmax',t,...
+    'netpair_file','D2t','record_spiking','off');
 
 
 %%adjust stimulus B strength
@@ -20,17 +20,10 @@ options = set_options('modeltype','NETS','comp_location','hpc',...
 %options.trial_stimuli(2) = options.trial_stimuli(2) * stim_mod; %adjust stim B
 
 
-%---need to get more states for slow nets here, new stim range---
-switch options.stim_targs
-    case 'Estay' %set to random slow network instead
-        slow_nets = 1:2:9;
-        do_config = slow_nets(randi(numel(slow_nets)));
-        options = get_network_params(do_config,options);
-end
-stim_mod = exp(1:.5:3); %new range for stim B
-stim_mod = randsample(stim_mod,1);
-options.trial_stimuli(2) = options.trial_stimuli(2) * stim_mod; %adjust stim B
-%-----------------------------
+%------test with stim found for network #1 
+options = get_network_params(1,options);
+Rstim = 68; %found in search 
+options.trial_stimuli = [Rstim,Rstim];
 
 
 %---run-----------------------
@@ -42,4 +35,17 @@ if isempty(dir(fullfile(options.save_dir,'code4*zip')))
 end
 delete(options.output_log) %no need for these right now
 
+
+% %if you need more states for specific things, use code here:
+% %---need to get more states for slow nets here, new stim range---
+% switch options.stim_targs
+%     case 'Estay' %set to random slow network instead
+%         slow_nets = 1:2:9;
+%         do_config = slow_nets(randi(numel(slow_nets)));
+%         options = get_network_params(do_config,options);
+% end
+% stim_mod = exp(1:.5:3); %new range for stim B
+% stim_mod = randsample(stim_mod,1);
+% options.trial_stimuli(2) = options.trial_stimuli(2) * stim_mod; %adjust stim B
+% %-----------------------------
 
