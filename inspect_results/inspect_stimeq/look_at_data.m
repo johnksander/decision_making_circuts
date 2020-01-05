48b9eb6fbe796c28af9c8a9d85db48be5eb5d76a
johnksander <john.ksander@gmail.com>
2020-01-05 15:24:23 -0500

    20200105

---
+++ inspect_results/inspect_stimeq/look_at_data.m
@@ -0,0 +1,60 @@
+clear
+clc
+format compact
+hold off;close all
+
+%This plots results from the stimulus equating jobs. 
+basedir = '~/Desktop/ksander/rotation/project/';
+curr_dir = fullfile(basedir,'inspect_results/inspect_stimeq');
+resdir = fullfile(basedir,'Results/equate_D2t_stims');
+RFN = dir(fullfile(resdir,'output_log_*.txt'));
+RFN = {RFN.name};
+
+pull_data = 'no';
+
+%net IDs, get the number at the end of output_log_i.txt
+NID = cellfun(@(x) strsplit(strrep(x,'.txt',''),'_'),RFN,'UniformOutput',false);
+NID = cellfun(@(x) str2double(x{end}),NID);
+
+%need to reformat these for the function below... 
+for idx = 1:numel(RFN)
+    f = fullfile(resdir,RFN{idx});
+    fnew = fullfile(curr_dir,['data-',RFN{idx}]);
+    switch pull_data
+        case 'yes'
+            cmd = sprintf('grep target %s > %s',f,fnew);
+            system(cmd);
+    end
+    plot_stims(fnew)
+    T = sprintf('network %i',NID(idx));
+    title(T,'FontSize',18,'FontWeight','b')
+end
+
+
+
+
+% N = 3;
+% for idx = 1:Nsystem(command)
+%     
+%     plot_stims(sprintf('data%i.txt',idx))
+%     
+% end
+
+
+
+function plot_stims(FN)
+
+%data = readtable('data1.txt','Delimiter','|');
+
+data = readtable(FN);
+
+stim = data.Var4;
+T = data.Var7;
+T = strrep(T,'s','');
+T = str2double(T);
+
+figure;
+scatter(stim,T,80,'red','filled')
+xlabel('stim Hz'); ylabel('time (s)')
+set(gca,'FontSize',18,'FontWeight','b')
+end
---
+++ parsweep_D2t_very_slow_driver.m
@@ -0,0 +1,41 @@
+clear 
+clc
+format compact 
+
+
+
+
+%my model 
+%---setup---------------------
+jID = str2num([getenv('SLURM_JOBID'), getenv('SLURM_ARRAY_TASK_ID')]);
+t = 1500; %trial simulation time (s) 
+options = set_options('modeltype','PS','comp_location','hpc',...
+    'sim_name','parsweep_D2t_very_slow_baseline','jobID',jID,'tmax',t,...
+    'ratelim_check','off','cut_leave_state',t,'noswitch_timeout',t);
+
+
+%---grid search
+Ngrid = 100;
+ItoE = linspace(0.1,12.5,Ngrid);
+EtoI = linspace(0,.75,Ngrid);
+[ItoE,EtoI] = meshgrid(ItoE,EtoI);
+ItoE = ItoE(:); EtoI = EtoI(:);
+%looking only for slow networks
+valid_range = ItoE >= 7.5 & EtoI >= .225;
+ItoE = ItoE(valid_range);
+EtoI = EtoI(valid_range);
+Gidx = str2num(getenv('SLURM_ARRAY_TASK_ID'));%HPCC only lets indicies up to 10k!!
+options.ItoE = ItoE(Gidx);
+options.EtoI = EtoI(Gidx);
+
+%---run-----------------------
+modelfile = spikeout_model(options);
+%---cleanup-------------------
+if isempty(dir(fullfile(options.save_dir,'code4*zip')))
+    driverfile = mfilename;
+    backup_jobcode(options,driverfile,modelfile)
+end
+%delete(options.output_log) %no need for these right now
+logdir = fullfile(options.save_dir,'logs'); %put them seperately
+if ~isdir(logdir),mkdir(logdir);end
+movefile(options.output_log,logdir)
